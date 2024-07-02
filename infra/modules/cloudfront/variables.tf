cb0a1fe95cfc2e5613dfa3a582fa9d74ec2b2d9c
sugiyama404 <mmingriha@gmail.com>
2024-07-02 09:53:43 +0900

    create ecs

--- infra/modules/cloudfront/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "app_name" {}
-variable "region" {}
-variable "s3_domain_name" {}
-variable "s3_origin_id" {}
-variable "api_gateway_origin_id" {}
-variable "api_gateway_domain_name" {}
--- infra/modules/s3/aws_s3_bucket_website_configuration.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket_website_configuration" "main" {
-  bucket = aws_s3_bucket.main.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
